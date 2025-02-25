3174ce72a6d48ecdc02ca8b4bb83e76125515e1a
tomohaya23 <tomohaya23@gmail.com>
2025-02-25 14:10:00 +0900

    feat: add load custom dataset(empty)

---
+++ src/load_custom_dataset.py
@@ -0,0 +1,7 @@
+def load_dataset(dataset_param: dict):
+    train_image_paths = []
+    train_labels = []
+    val_image_paths = []
+    val_labels = []
+
+    return train_image_paths, train_labels, val_image_paths, val_labels
