bc59a17759a72ff22777924e188aa707a3d29d74
tomohaya23 <tomohaya23@gmail.com>
2025-02-26 15:03:16 +0900

    fix: variable name etc

--- src/load_datasets.py
+++ src/load_datasets.py
@@ -1,27 +1,25 @@
-def load_datasets(datasets_info):
+def load_datasets(config_datasets):
     train_data = []
     train_labels = []
     val_data = []
     val_labels = []
 
-    for dataset in datasets_info:
-        if dataset["name"] == "coco":
+    for config_dataset in config_datasets["info"]:
+        if config_dataset["name"] == "coco":
             from load_coco_dataset import load_dataset
 
-            train_coco_data, train_coco_labels = load_dataset(dataset, "train")
-            val_coco_data, val_coco_labels = load_dataset(dataset, "val")
+            train_coco_data, train_coco_labels = load_dataset(config_dataset, "train", config_datasets["class_to_id"])
+            val_coco_data, val_coco_labels = load_dataset(config_dataset, "val", config_datasets["class_to_id"])
             train_data.extend(train_coco_data)
             train_labels.extend(train_coco_labels)
             val_data.extend(val_coco_data)
             val_labels.extend(val_coco_labels)
 
-        if dataset["name"] == "custom_dataset":
+        if config_dataset["name"] == "custom":
             from load_custom_dataset import load_dataset
 
-            train_custom_data, train_custom_labels, val_custom_data, val_custom_labels = load_dataset(dataset)
+            train_custom_data, train_custom_labels = load_dataset(config_dataset, config_datasets["class_to_id"])
             train_data.extend(train_custom_data)
             train_labels.extend(train_custom_labels)
-            val_data.extend(val_custom_data)
-            val_labels.extend(val_custom_labels)
 
     return train_data, train_labels, val_data, val_labels
