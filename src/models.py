6dfe470c283b368c117e8077ae6834306ed23ada
tomohaya23 <tomohaya23@gmail.com>
2025-03-22 13:17:08 +0900

    fix: model arch

--- src/models.py
+++ src/models.py
@@ -1,11 +1,18 @@
-import custom_model
+import yolox
+
 
 def create_model(model_size: str, num_classes: int):
-    if model_size == "small":
-        return custom_model.yolo(num_classes)
+    if model_size == "nano":
+        return yolox.yolox_n(num_classes)
+    elif model_size == "tiny":
+        return yolox.yolox_t(num_classes)
+    elif model_size == "small":
+        return yolox.yolox_s(num_classes)
     elif model_size == "medium":
-        return None
+        return yolox.yolox_m(num_classes)
     elif model_size == "large":
-        return None
+        return yolox.yolox_l(num_classes)
+    elif model_size == "xlarge":
+        return yolox.yolox_x(num_classes)
     else:
-        return None
+        return yolox.yolox_n(num_classes)
